6dfdf8e290ab583bd4cfe73ddd5128312beed291
ewnd9 <ewndnine@gmail.com>
2015-07-12 15:50:12 +0200

    Add progress bar
    Fixes #1 and kevva/brightness#1.

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 'use strict';
 var brightness = require('brightness');
 var meow = require('meow');
+var progressControl = require('progress-control');
 
 var cli = meow({
 	help: [
@@ -13,13 +14,42 @@ var cli = meow({
 
 try {
 	if (!cli.input.length) {
-		brightness.get(function (err, data) {
+		brightness.get(function (err, val) {
 			if (err) {
 				console.error(err.message);
 				process.exit(1);
 			}
 
-			console.log(data);
+			brightness.set(val, function (err) {
+				if (err) {
+					console.error(err.message);
+					process.exit(1);
+				}
+
+				function updateBar(val) {
+					brightness.set(val, function (err) {
+						if (err) {
+							console.error(err.message);
+							process.exit(1);
+						}
+					});
+
+					bar.update(val, {val: val});
+				}
+
+				var bar = progressControl('Use up/down arrows [:bar] :val ', {total: 10}, {
+					up: function () {
+						val = Math.min(Math.round((val + 0.1) * 10) / 10, 1);
+						updateBar(val);
+					},
+					down: function () {
+						val = Math.max(Math.round((val - 0.1) * 10) / 10, 0);
+						updateBar(val);
+					}
+				});
+
+				updateBar(val);
+			});
 		});
 
 		return;
